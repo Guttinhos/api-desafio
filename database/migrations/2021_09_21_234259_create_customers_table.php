56de5892ca507cc3f34e80e017a014a6a4527354
Gutemberg Mauricio <gutem.mauricio@gmail.com>
2021-09-21 21:15:57 -0300

    criada migração de clientes

---
+++ database/migrations/2021_09_21_234259_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 100);
+            $table->string('cpf', 14);
+            $table->string('rg', 20);
+            $table->date('birth_date');
+            $table->string('phones', 255);
+            $table->char('birth_state', 2);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
